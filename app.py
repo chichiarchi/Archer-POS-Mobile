e032e30ddd94f9a0b2e26cfff10245cf33e1be79
Arvin <aclatagan15@gmail.com>
2026-05-05 14:54:00 +0800

    feat: imrpovements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QVBoxLayout, QWidget
-from PySide6.QtGui import QShortcut, QKeySequence
+from PySide6.QtGui import QShortcut, QKeySequence, QIcon
 import database
 from pos_module import POSModule
 from inventory_module import InventoryModule
@@ -13,7 +13,8 @@ class ArcherPOS(QMainWindow):
     def __init__(self, user_role="staff"):
         super().__init__()
         self.user_role = user_role
-        self.setWindowTitle("Archer POS v2 - Dashboard")
+        self.setWindowTitle("Archer POS v2 - Dashboard") 
+        self.setWindowIcon(QIcon("archer_logo.png"))
         self.showMaximized()
         self.setup_ui()
 
@@ -188,23 +189,23 @@ class ArcherPOS(QMainWindow):
 
         # Tab 1: Dashboard
         self.dashboard_tab = DashboardModule(self.user_role)
-        self.tabs.addTab(self.dashboard_tab, "Dashboard (F1)")
+        self.tabs.addTab(self.dashboard_tab, QIcon("archer_logo.png"), "Dashboard (F1)")
 
         # Tab 2: POS
         self.pos_tab = POSModule(self.user_role)
-        self.tabs.addTab(self.pos_tab, "Point of Sale (F2)")
+        self.tabs.addTab(self.pos_tab, QIcon("archer_logo.png"), "Point of Sale (F2)")
 
         # Tab 3: Inventory
         self.inventory_tab = InventoryModule(self.user_role)
-        self.tabs.addTab(self.inventory_tab, "Stock Manager (F3)")
+        self.tabs.addTab(self.inventory_tab, QIcon("archer_logo.png"), "Stock Manager (F3)")
 
         # Tab 4: Balance
         self.balance_tab = BalanceModule(self.user_role)
-        self.tabs.addTab(self.balance_tab, "Balance Manager (F5)")
+        self.tabs.addTab(self.balance_tab, QIcon("archer_logo.png"), "Balance Manager (F5)")
 
         # Tab 5: Data Logs
         self.logs_tab = LogsModule(self.user_role)
-        self.tabs.addTab(self.logs_tab, "Data Logs (F6)")
+        self.tabs.addTab(self.logs_tab, QIcon("archer_logo.png"), "Data Logs (F6)")
 
         # # Tab 6: Settings
         # self.settings_tab = SettingsModule(self.user_role)
